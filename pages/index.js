c9bf7f7b93e2298c8f7adef7c849f98899bb96b9
Hiratake <hiratake@hiratake.xyz>
2021-09-04 15:57:17 +0900

    chore: 事前レンダリング

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@ import Layout, { siteTitle } from '../components/layout'
 
 import UtilStyle from '../styles/utils.module.css'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,6 +27,21 @@ export default function Home() {
           This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.
         </p>
+      </section>
+
+      <section className={`${UtilStyle.headingMd} ${UtilStyle.padding1px}`}>
+        <h2 className={UtilStyle.headingLg}>Blog</h2>
+        <ul className={UtilStyle.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={UtilStyle.listItem} key={id}>
+              { title }
+              <br />
+              { id }
+              <br />
+              { date }
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
