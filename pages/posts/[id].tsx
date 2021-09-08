9d5f6dbb323342c2968e68b18a1ee86041d88587
Hiratake <hiratake@hiratake.xyz>
2021-09-08 20:53:36 +0900

    chore: ファイルをtypescriptに変更してみる

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -15,7 +15,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const postData = await getPostData(params.id as string)
   return {
     props: {
       postData,
@@ -23,7 +23,15 @@ export async function getStaticProps({ params }) {
   }
 }
 
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
